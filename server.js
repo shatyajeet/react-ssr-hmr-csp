c9e74929a5e86e1dacc5449e68647ef1f6d4e1a1
Satyajeet Parida <shatyajeet@gmail.com>
2017-02-22 17:06:09 +0530

    Added proper logging for express server

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+import ip from 'ip';
 import express from 'express';
+import minimist from 'minimist';
 
 const env = process.env.NODE_ENV;
+const argv = minimist(process.argv.slice(2));
+
+const customHost = argv.host || process.env.HOST;
+const host = customHost || null;
+const prettyHost = customHost || 'localhost';
+
+const port = argv.port || process.env.PORT || 3000;
+
 const app = express();
 
-app.listen(3000, '0.0.0.0', (err, a, b, c) => {
+app.listen(port, host, (err) => {
   if (err) {
     console.error(err.message);
   } else {
-    console.info('Listening in at http://localhost:3000');
+    console.info(`Listening at
+      Localhost: http://${prettyHost}:${port}
+      LAN: http://${ip.address()}:${port}
+    `);
   }
 });
